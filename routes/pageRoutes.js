0bf14dbed27f674ddfb36310558c821c126d868f
MiniGar210 <Neil.nothnagel@gmail.com>
2025-05-14 16:27:10 +0200

    resending to make sure its there

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -4,18 +4,29 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    const data = {title: 'home'};
+    res.render('Pages/home',data);
 });
 
+
 router.get('/about', (req, res) => {
+    const data = {title: 'about'};
+    res.render('pages/about',data);
 });
 
 router.get('/events', (req, res) => {
+    const data = {title: 'events'};
+    res.render('pages/events',data);
 });
 
 router.get('/contact', (req, res) => {
+    const data = {title: 'contact'};
+    res.render('pages/contact',data);
 });
 
 router.get('/thankyou', (req, res) => {
+    const data = {title: 'thankyou'};
+    res.render('pages/thankyou',data);
 });
 
 module.exports = router;
